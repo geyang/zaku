d118086727cc051fd368ccd0986245462dc3c474
Ge Yang <yangge1987@gmail.com>
2024-04-21 17:25:34 -0400

    move the autoattributes up, got repeated

--- zaku/client.py
+++ zaku/client.py
@@ -43,13 +43,25 @@ class TaskQ(PrefixProto, cli=False):
             "ttl": 5.0
         }
 
-    .. automethod::init_queue
-    .. automethod::add
-    .. automethod::take
-    .. automethod::mark_done
-    .. automethod::mark_reset
-    .. automethod::pop
-    .. automethod::clear_queue
+    Task Queue Configurations
+    +++++++++++++++++++++++++
+
+    .. autoattribute:: uri
+    .. autoattribute:: name
+    .. autoattribute:: ttl
+    .. autoattribute:: no_init
+
+    Task Life-cycle Methods
+    +++++++++++++++++++++++
+
+    .. automethod:: init_queue
+    .. automethod:: add
+    .. automethod:: take
+    .. automethod:: mark_done
+    .. automethod:: mark_reset
+    .. automethod:: pop
+    .. automethod:: clear_queue
+
     """
 
     uri: str = Proto(
