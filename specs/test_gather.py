667fb21afc0f13e09609b60c0b511e9219abbbff
Ge Yang <yangge1987@gmail.com>
2024-06-01 00:34:19 -0400

    bump version to 0.0.15, add additional test to gather

--- specs/test_gather.py
+++ specs/test_gather.py
@@ -16,7 +16,7 @@ def worker_process(queue_name):
     for i in range(10):
         with queue.pop() as job:
             if job is None:
-                sleep(0.1)
+                sleep(0.5)
                 continue
 
             # these are boilerplates that we prob want to remove from the user's space.
@@ -26,8 +26,8 @@ def worker_process(queue_name):
             gather_token = job.pop("_gather_token")  # _gather_token must be in.
 
             # we simulate a long-running job. Make sure you clear the queue first though.
-            sleep(0.01)
-
+            sleep(0.1)
+            # print(job['seed'])
         # we return the result to the response topic.
         # can be called return gather or something
         # print("finished job", job)
@@ -37,7 +37,7 @@ def worker_process(queue_name):
     print("process finished")
 
 
-@pytest.mark.dependency(name="test_rpc")
+@pytest.mark.dependency(name="test_gather")
 def test_gather():
     """adding"""
     from multiprocessing import Process
@@ -68,30 +68,49 @@ def test_gather():
     print("waiting...")
     assert not is_done(blocking=False), "done should not be marked to be True"
 
+    for p in procs:
+        p.join()
 
-@pytest.mark.dependency(name="test_rpc")
+@pytest.mark.dependency(name="test_gather_imperative")
 def test_gather_imperative():
     """adding"""
+    import time
     from multiprocessing import Process
+
+    print()
+
+    print("setting up queue")
 
     queue_name = "ZAKU_TEST:debug-gather-queue"
     job_queue = TaskQ(name=queue_name, uri="http://localhost:9000")
     # this is important, otherwise the worker will get suck with
-    # an old message.
+    # # an old message.
     job_queue.clear_queue()
+
+    print("this is here")
 
     # start five worker processes
     procs = []
-    for i in range(40):
+    for i in range(4):
         p = Process(target=worker_process, args=(queue_name,))
         p.start()
         procs.append(p)
 
-    jobs = [dict(seed=i) for i in range(100)]
+    print("===================")
+    jobs = [dict(seed=i) for i in range(30)]
     tokens = None
     for j in jobs:
         is_done, tokens = job_queue.gather_one(j, tokens)
-
+    print("done adding")
     print("waiting...")
+
+    while not is_done(blocking=False):
+        print("counts", len(tokens), "total left", len(job_queue))
+        time.sleep(0.1)
+
+    print("job_queue length:", len(job_queue))
     assert is_done(blocking=True), "done should mark to be True"
     print("is done!")
+
+    for p in procs:
+        p.join()
